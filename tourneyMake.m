1bc47c272b3bc811e31238ddeb3d3eddbbebdf9b
Fluvio Lobo <flobo@ist.ucf.edu>
2016-01-05 12:14:23 -0500

    The innermost loop of the algorithm has been expanded to include multiple weeks or match repetitions

--- tourneyMake.m
+++ tourneyMake.m
@@ -15,9 +15,8 @@
 clear all;
 clc;
 
-Nteams = 10;
-Nweeks = 2;
-
+Nteams = 6;
+Nweeks = 3;
 
 TeamNumList = 1:1:Nteams;
 
@@ -25,19 +24,68 @@ Schedule = zeros(Nteams/2,2,Nweeks);
 
 for h = 1:Nweeks
     
-    data = TeamNumList;
+    data0 = TeamNumList;
+    
+    if h == 1 % only for the first week
 
-    for i = 1:Nteams/2
+        for i = 1:Nteams/2
+
+            for j = 1:2 % number of teams per game (only works with two)
+
+                pick = datasample(data0,1); % first pick, first round
+
+                index0 = find(data0 == pick);
+                data0(index0) = [];
+
+                Schedule(i,j,h) = pick;
+
+            end % end of for-loop
+
+        end % end of for-loop
         
-        for j = 1:2
+    elseif h > 1
+        
+        for i = 1:Nteams/2
+        
+            pick = datasample(data0,1);
+            index0 = find(data0 == pick);
+            data0(index0) = [];
 
-            Schedule(i,j,h) = datasample(data,1);
-
-            index = find(data == Schedule(i,j,h));
-            data(index) = [];
+            Schedule(i,1,h) = pick;
             
+            % now find the previous matches
+            % index1 = zeros(h-1,2);
+            data1 = data0;
+            for k = 1:h-1
+
+                [index1(k,1), index1(k,2)] = find(Schedule(:,:,k) == pick);
+
+                if index1(k,2) == 1
+
+                    index2 = 2;
+
+                elseif index1(k,2) == 2
+
+                    index2 = 1;
+
+                end
+
+                index3 = find(data1 == Schedule(index1(k,1),index2,k));
+                data1(index3) = [];
+
+            end % end of "k" for-loop
+            
+            pick = datasample(data1,1);
+            Schedule(i,2,h) = pick;
+
+            index0 = find(data0 == pick);
+            data0(index0) = []; 
+
         end
 
-    end % end of for-loop
+    end
     
 end % end of for-loop
+
+    
+ Schedule
