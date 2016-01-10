fba52e1f012e6a1df75536c640d862846756c19a
Fluvio Lobo <flobo@ist.ucf.edu>
2016-01-10 17:17:28 -0500

    The function tourneyMakeFast uses a different approach than the origianal tourneyMake function. In this case, the computer calculates the possible match combination initially, then organizes the combinations so that matches do not repeat, etc.

---
+++ tourneyMakeFast.m
@@ -0,0 +1,58 @@
+%% SOTECH CONSULTING LLC
+%
+% Title: Tournament Structure Generator
+% Author: Fluvio L Lobo Fenoglietto
+% Date: 01/04/2016
+%
+% Description:
+%
+%
+%
+
+clear all
+clc
+
+N_teams = 6;
+N_weeks = 5;
+
+N_teams_per_match = 2; % fixed for soccer
+
+TeamNumList = 1:1:N_teams;
+
+Match_Combinations = nchoosek(TeamNumList,2);
+N_combinations = length(Match_Combinations);
+N_matches_per_week = N_combinations/N_weeks;
+
+Schedule = zeros(N_matches_per_week, 2, N_weeks);
+
+D0 = Match_Combinations;
+
+for h = 1:2%N_weeks
+    
+    D1 = D0;
+    
+    for i = 1:N_matches_per_week
+        
+        % -- Pick --
+        [pick, D1] = PickRandMatchCombination(D1);
+        pick
+        D1
+        
+        % -- Eliminate Pick from Combinations --
+        
+        [D0] = RemovePrevPickFromMatchCombinations(pick,D0);
+
+        % -- Store Pick --
+        
+        Schedule(i,:,h) = pick;
+        
+        % -- Edit Array --
+        
+        [D1] = RemovePrevTeamFromWeekMatches(pick,D1);
+        D1
+
+        Schedule(:,:,h);
+        
+    end % end of "matches" or "i" loop
+
+end % end of "weeks" or "h" loop
