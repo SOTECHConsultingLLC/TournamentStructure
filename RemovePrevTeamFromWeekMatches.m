b48bc414c7ca3227c4ae782545f949fd36ddb2f3
Fluvio Lobo <flobo@ist.ucf.edu>
2016-01-10 17:23:41 -0500

    This function removes the most recent randomly pick match combination from the list of possible matches for the corresponding week. This function also eliminates both teams from the list, avoiding any back-to-back games.

---
+++ RemovePrevTeamFromWeekMatches.m
@@ -0,0 +1,49 @@
+%% SOTECH CONSULTING LLC
+%
+% Title: Remove Previous Team From Week Matches
+% Author: Fluvio L Lobo Fenoglietto
+% Date: 01/05/2016
+%
+% Description:
+% This function removes the teams listed on the previous match, from the
+% following (remainder) matches.
+%
+
+function [Z] = RemovePrevTeamFromWeekMatches(pick,X)
+
+    Y = X;
+
+    SizeOfX = size(X);
+    N_combinations = SizeOfX(1,1);
+    N_teams_per_match = SizeOfX(1,2);
+    pickLen = length(pick);
+
+    for j = 1:N_combinations
+
+        for k = 1:N_teams_per_match
+
+            for l = 1:pickLen
+
+                if X(j,k) == pick(l)
+
+                    Y(j,:) = [0, 0];
+
+                end
+
+            end
+
+        end
+
+    end
+
+    indeces = find(Y(:,1) ~= 0);
+
+    Z = zeros(length(indeces),2);
+
+    for j = 1:length(indeces)
+
+        Z(j,:) = Y(indeces(j),:);
+
+    end  
+
+end % end of function
