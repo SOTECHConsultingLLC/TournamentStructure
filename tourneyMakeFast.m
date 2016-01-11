0bc16f05e5b8959b18d3037e344c7617b8a41712
Fluvio Lobo <flobo@ist.ucf.edu>
2016-01-10 20:56:27 -0500

    Added a try-catch statement to enclose an area of possible error. Given to the consistency of the error, and its nature, the try-catch allows for a while loop to repeat the section of the code until an appropriate solution is found

--- tourneyMakeFast.m
+++ tourneyMakeFast.m
@@ -25,34 +25,60 @@ N_matches_per_week = N_combinations/N_weeks;
 
 Schedule = zeros(N_matches_per_week, 2, N_weeks);
 
-D0 = Match_Combinations;
+flag = 0;
+iter = 1;
+iterMax = 100;
 
-for h = 1:2%N_weeks
+while flag == 0
     
-    D1 = D0;
+    try % execute random selection protocol until criteria is met
+
+        D0 = Match_Combinations;
+
+        for h = 1:N_weeks
+
+            D1 = D0;
+
+            for i = 1:N_matches_per_week
+
+                % -- Pick --
+                [pick, D1] = PickRandMatchCombination(D1);
+
+                % -- Eliminate Pick from Combinations --
+
+                [D0] = RemovePrevPickFromMatchCombinations(pick,D0);
+
+                % -- Store Pick --
+
+                Schedule(i,:,h) = pick;
+
+                % -- Edit Array --
+
+                [D1] = RemovePrevTeamFromWeekMatches(pick,D1);
+
+                Schedule(:,:,h);
+
+            end % end of "matches" or "i" loop
+
+
+        end % end of "weeks" or "h" loop
+        
+        flag = 1;
+        
+    catch
+        
+        flag = 0;
+        
+    end
     
-    for i = 1:N_matches_per_week
+    iter = iter + 1;
+    
+    if iter == iterMax
         
-        % -- Pick --
-        [pick, D1] = PickRandMatchCombination(D1);
-        pick
-        D1
+        flag == 2;
         
-        % -- Eliminate Pick from Combinations --
+        disp('Program Failed!');
         
-        [D0] = RemovePrevPickFromMatchCombinations(pick,D0);
-
-        % -- Store Pick --
-        
-        Schedule(i,:,h) = pick;
-        
-        % -- Edit Array --
-        
-        [D1] = RemovePrevTeamFromWeekMatches(pick,D1);
-        D1
-
-        Schedule(:,:,h);
-        
-    end % end of "matches" or "i" loop
-
-end % end of "weeks" or "h" loop
+    end
+    
+end
